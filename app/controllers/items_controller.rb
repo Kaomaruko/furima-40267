46f08ce84250ba1079fdf4a5ecc27c86b1c1807a
Kaomaruko <kaori.mikan1111@gmail.com>
2024-02-12 16:59:24 +0900

    商品詳細表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,11 +2,14 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   
 
+  
+
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def new
